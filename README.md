edcbd5a071ac220442a7ccc932999fafefb641b2
Miguel Barrios <mbarrios@enthec.com>
2022-10-12 23:00:26 +0200

    New Project Fast Api Docker

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Detect-Lang-Api
+# Deploy ML models with FastAPI, Docker, and Heroku
 In this project we intend to develop a language detector in such a way that a client can receive the answers through the Api Rest service.
---
+++ app/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.openapi.models import Response
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from app.model.model import predict_pipeline
+
+app = FastAPI()
+
+__version__ = "0.1.0"
+
+
+class PrectionOut(BaseModel):
+    text: str
+
+
+class TextIn(BaseModel):
+    text: str
+
+
+@app.get("/")
+def home():
+    response = {
+        "HealthCheck": "Ok",
+        "Version": __version__
+
+    }
+    return JSONResponse(content=response, media_type="application/json")
+
+
+@app.post("/predict", response_model=BaseModel)
+def predict(payload: TextIn):
+    lang = predict_pipeline(payload)
+    response = {
+        "language": lang
+    }
+    return JSONResponse(content=response, media_type="application/json")
